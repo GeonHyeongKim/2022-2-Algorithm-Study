8f2fef511a9fd5f11c6ae09f5e61152ccd80c831
ChoiysApple <chcs1370@gmail.com>
2022-08-17 21:37:27 +0900

    Update 실패율.swift

--- src/daegun/week1/실패율.swift
+++ src/daegun/week1/실패율.swift
@@ -1,62 +1,49 @@
 import Foundation
 
-func solution(_ numbers:[Int], _ hand:String) -> String {
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     
-    var result: [String] = []
+    // 각 스테이지별 실패율을 저장하는 변수
+    var failures: [Int:Float] = [:]
+    (1...N).forEach { failures[$0] = 0 }
     
-    // 키패드 번호 위치 좌표
-    let location: [String: (Int, Int)] = [
-        "1": (0,0), "2": (1,0), "3": (2,0),
-        "4": (0,1), "5": (1,1), "6": (2,1),
-        "7": (0,2), "8": (1,2), "9": (2,2),
-        "*": (0,3), "0": (1,3), "#": (2,3),
-    ]
-    var leftHand = location["*"]!    // 왼손 위치
-    var rightHand = location["#"]!   // 오른손 위치
+    // stages 내림차순 정렬
+    var sortedStages = stages.sorted(by: >)
     
-    for number in numbers {
+    var prev = -1           // 이전 element 값 (현재 값과 같은지 비교)
+    var currentCount = 0    // 중복되는 숫자 개수 카운팅
+    for (index, stage) in sortedStages.enumerated() {
         
-        // 눌러야할 키패드 좌표
-        let target = location[String(number)]!
-        
-        // 무조건 왼손
-        if target.0 == 0 {
-            result.append("L")
-            leftHand = target
-        
-        // 무조건 오른손
-        } else if target.0 == 2 {
-            result.append("R")
-            rightHand = target
+        // 중복 끝남
+        if prev != stage {
             
-        // 거리에 따라 결정
-        } else {
-            
-            let fromLeft = abs(leftHand.0 - target.0) + abs(leftHand.1 - target.1)
-            let fromRight = abs(rightHand.0 - target.0) + abs(rightHand.1 - target.1)
-            
-            // 오른손이 더 가깝다
-            if fromLeft > fromRight {
-                result.append("R")
-                rightHand = target
-                
-            // 거리 동일
-            } else if fromLeft == fromRight {
-                if hand == "left" {
-                    result.append("L")
-                    leftHand = target
-                } else {
-                    result.append("R")
-                    rightHand = target
-                }
-            // 왼손이 더 가깝다
-            } else {
-                result.append("L")
-                leftHand = target
+            if currentCount > 0 && prev <= N {
+                failures[prev] = Float(currentCount)/Float(index)
             }
+            
+            // prev, currentCount 초기화
+            currentCount = 1
+            prev = stage
+
+        } else {
+            currentCount += 1
         }
     }
     
+    // 마지막 결과 가지고 한번 더 연산
+    if currentCount > 0 && prev <= N {
+        failures[prev] = Float(currentCount)/Float(sortedStages.count)
+    }
     
-    return result.joined(separator: "")
+    print(failures)
+
+    // 결과 정렬 후 key 추출
+    let result = failures.sorted {
+        if $0.value == $01.value {
+            return $0.key < $1.key
+        } else {
+            return $0.value > $1.value
+        }
+    }.map { $0.key }
+    
+    return result
 }
